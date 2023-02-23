3d8ba2d14b0d72f11dffe7b4695f76c64d441516
Kuba <jaknowa2000@gmail.com>
2023-02-23 17:07:59 +0100

    Declaration of classes SmallSquare and RowOrColumn

--- Solver_cpp/sudoku_board.cpp
+++ Solver_cpp/sudoku_board.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<memory> //inteligentne wskazniki
+#include <typeinfo> //pomocne
 
 #include "sudoku_board.h"
 
@@ -9,7 +11,28 @@ Field::Field(){
     possible_values = {};
 }
 
+// zeby nie dawac public klasy skojarzone
+
+
+
+SmallSquare::SmallSquare(){
+    for(auto& row: small_square){
+        for(auto& item : row){
+            item = std::make_shared<Field>();
+        }
+    }
+};
+
+
+RowOrColumn::RowOrColumn(){
+    for(auto& item : row_or_column){
+        item = std::make_shared<Field>();
+        std::cout<<item->value;
+    }
+};
 
 int main(){
+    auto sml1 = std::make_shared<SmallSquare>();
+    auto sml2 = std::make_shared<RowOrColumn>();
     return 0;
 }
--- Solver_cpp/sudoku_board.h
+++ Solver_cpp/sudoku_board.h
@@ -5,11 +5,26 @@
 
 
 class Field{
+    public:
     int value;
     bool isset;
     std::vector<int> possible_values;
-    protected:
+    // protected: rather not
     Field();
+};
+
+class SmallSquare{
+    //tu utworzyc
+    public:
+    std::shared_ptr<Field> small_square[3][3];
+
+    SmallSquare();
+};
+
+class RowOrColumn{
+    public:
+    std::shared_ptr<Field> row_or_column[9];
+    RowOrColumn();
 };
 
 #endif
