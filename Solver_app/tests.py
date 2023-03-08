19453ff4e7ca4f6a72998ef5627f05f03ff7a37b
Kuba <jaknowa2000@gmail.com>
2023-03-08 21:09:53 +0100

    Functional and unit tests for redirecting to solver page.

--- Solver_app/tests.py
+++ Solver_app/tests.py
@@ -5,3 +5,9 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+
+class SolverPageTest(TestCase):
+    def test_uses_solver_template(self):
+        response = self.client.get('/solver/')
+        self.assertTemplateUsed(response, 'solver.html')
