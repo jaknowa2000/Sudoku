898cb49c17fa92ed179b227d8e20098b1d455e41
Kuba <jaknowa2000@gmail.com>
2023-03-13 15:25:18 +0100

    URL and View for solved sudoku puzzle.

--- Solver_app/urls.py
+++ Solver_app/urls.py
@@ -5,5 +5,6 @@ app_name = 'Solver_app'
 
 urlpatterns = [
     path('', views.solver, name='solver'),
-    path('new', views.new_sudoku_puzzle, name='new_puzzle'),
+    path('new', views.new_sudoku_puzzle, name='new_sudoku_puzzle'),
+    path('<str:board_id>/', views.solved_board, name='solved_board'),
 ]
--- Solver_app/views.py
+++ Solver_app/views.py
@@ -14,3 +14,8 @@ def new_sudoku_puzzle(request):
     post_request_list = list(map(int, request.POST.getlist('fields')))
     sudoku_puzzle = SudokuBoard.objects.create(fields=post_request_list)
     return redirect(f'/solver/{sudoku_puzzle.id}/')
+
+
+def solved_board(request, board_id):
+    puzzle = SudokuBoard.objects.get(id=board_id)
+    return render(f'/solver/{board_id}', 'solved.html', {'board': puzzle})
