b8692b7cd144c03abec9cb8a18d3113e7c5ae153
Kuba <jaknowa2000@gmail.com>
2023-03-13 14:04:09 +0100

    Test for New Sudoku Board Puzzle.

--- Solver_app/tests.py
+++ Solver_app/tests.py
@@ -19,12 +19,26 @@ class SudokuBoardModelTest(TestCase):
         sudoku_board = SudokuBoard()
         self.assertEqual(len(sudoku_board.fields), 81, "Wrong number of sudoku fields")
 
-    def test_board_can_can_fetch_and_save_elements_in_fields(self):
+    def test_board_can_fetch_and_save_elements_in_fields(self):
         sudoku_board = SudokuBoard()
-        sudoku_board_example = [[i for i in range(10)] + [i for i in range(9, 0, -1)] + [7] * 9] * 3
-        for index, value in enumerate(sudoku_board_example):
-            sudoku_board.fields[index] = value
+        sudoku_board_example = [i for i in range(1, 10)] + [i for i in range(9, 0, -1)] + [7] * 9
+        sudoku_board_example *= 3
+        sudoku_board.fields = sudoku_board_example
         sudoku_board.save()
         saved_sudoku_board = SudokuBoard.objects.first()
         self.assertEqual(saved_sudoku_board.fields, sudoku_board.fields)
+        self.assertEqual(saved_sudoku_board.fields, sudoku_board_example)
+
+
+class NewSodokuBoardTest(TestCase):
+    def test_can_save_POST_request(self):
+        sudoku_board_example = [i for i in range(1, 10)] + [i for i in range(9, 0, -1)] + [7] * 9
+        sudoku_board_example *= 3
+        sudoku_board_data = {'fields': sudoku_board_example}
+
+        self.client.post('/solver/new', data=sudoku_board_data)
+
+        self.assertEqual(SudokuBoard.objects.count(), 1, "The board didn't crate")
+        new_sudoku_board = SudokuBoard.objects.first()
+        self.assertEqual(new_sudoku_board.fields, sudoku_board_example)
 
